f3f1d6df25a4b11a83ccfe5881c99a7949e8b7d1
uliana5858 <118607939+uliana5858@users.noreply.github.com>
2022-12-05 17:29:13 +0900

    add prog

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Homework 1
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(console.ReadLine);
+
+int num1 = "2";
+int num2 = "3";
+int num3 = "7";
+
+int num (max) = int num1
+*/
+
+/*
+Homework 2
+
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int num1, num2, num3;
+Console.WriteLine("Enter First num: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second num: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third num: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+ if (num1 > max) max = num1;
+ if (num2 > max) max = num2;
+ if (num3 > max) max = num3;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+*/
+
+
+/*
+Homework 3
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int number;
+Console.WriteLine("Enter num:");
+number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0)
+
+    Console.WriteLine("Even number ");
+else 
+
+    Console.WriteLine("Odd number ");
+*/
+
